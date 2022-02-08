4e72cb5b4996a65a4cb19865400571b2d79ac722
take-2405 <take12.wa@icloud.com>
2022-02-08 23:37:08 +0900

    fix attach cors

--- presentation/router/router.go
+++ presentation/router/router.go
@@ -4,6 +4,7 @@ import (
 	"bit-board-auth/presentation/controller"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"net/http"
 	//middleware2 "prac-orm-transaction/presentation/middleware"
 	"time"
@@ -23,6 +24,17 @@ func NewServer() *Server {
 func (s *Server) Routing(uh controller.UserHandler) {
 	s.Router.Use(middleware.Timeout(60 * time.Second))
 	s.Router.Use(middleware.Logger)
+	s.Router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
 	s.Router.Route("/sign", func(api chi.Router) {
 		api.Route("/up", func(signup chi.Router) {
 			signup.Post("/", uh.SignUp())
