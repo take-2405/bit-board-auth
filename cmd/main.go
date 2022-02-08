96182825e712fe973bc21b6c96af98da803a9e9f
take-2405 <take12.wa@icloud.com>
2022-02-09 00:47:37 +0900

    delete 動作確認用のコードを削除

--- cmd/main.go
+++ cmd/main.go
@@ -4,19 +4,10 @@ import (
 	"bit-board-auth/config"
 	"bit-board-auth/di"
 	router2 "bit-board-auth/presentation/router"
-	"github.com/joho/godotenv"
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	err := godotenv.Load("./.env")
-	if err != nil {
-		log.Fatal()
-	}
-	message := os.Getenv("FS_PRIVATE_KEY")
-	log.Println(message)
 
 	//ルーターを初期化
 	router := router2.NewServer()
